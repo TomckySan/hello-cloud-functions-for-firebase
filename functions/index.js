9c598620d2558d806010c7e92477010bf8ead07b
Tomoka Baba <baba@tofulab.jp>
2018-09-23 20:13:57 +0900

    Add addMessage function.

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,11 @@ admin.initializeApp();
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+exports.addMessage = functions.https.onRequest((req, res) => {
+  const original = req.query.text;
+
+  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+    return res.redirect(303, snapshot.ref.toString());
+  });
+});
